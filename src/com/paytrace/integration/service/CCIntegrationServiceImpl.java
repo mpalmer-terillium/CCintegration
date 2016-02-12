5aa09e27b744e42279d5cfcf5b2fd0ff9269d472
mpalmer <mpalmer@terillium.com>
2016-02-12 15:48:50 -0600

    named parameter

--- src/com/paytrace/integration/service/CCIntegrationServiceImpl.java
+++ src/com/paytrace/integration/service/CCIntegrationServiceImpl.java
@@ -30,7 +30,7 @@ public class CCIntegrationServiceImpl implements CCIntegrationService {
      */
     @Override
     @WebMethod
-    public IntegrationResponse processExternalRequest(@WebParam(name = "arg0") ExternalValueObject evo) {
+    public IntegrationResponse processExternalRequest(@WebParam(name = "externalRequest") ExternalValueObject evo) {
         
         InternalIntegrationProcessor processor = new InternalIntegrationProcessor(IntegrationUtility.initVO(evo));
         IntegrationResponse response;
